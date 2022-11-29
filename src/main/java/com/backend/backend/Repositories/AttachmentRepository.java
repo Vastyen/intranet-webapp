d32296c5f4dc0867ab6eb3f2864e87ae1949c98d
Bastián Escribano <bastian.escribano@usach.cl>
2022-11-28 23:38:31 -0300

    (Bastián Escribano)[Feature] - Se agrega el post y get de subir archivo.

---
+++ src/main/java/com/backend/backend/Repositories/AttachmentRepository.java
@@ -0,0 +1,8 @@
+package com.backend.backend.Repositories;
+import com.backend.backend.Entities.Attachment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AttachmentRepository extends JpaRepository<Attachment, String> {
+}
---
+++ src/main/java/com/backend/backend/Services/AttachmentService.java
@@ -0,0 +1,9 @@
+package com.backend.backend.Services;
+
+import com.backend.backend.Entities.Attachment;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface AttachmentService {
+    Attachment saveAttachment(MultipartFile file) throws Exception;
+    Attachment getAttachment(String fileId) throws Exception;
+}
